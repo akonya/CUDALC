9a16838e28f44dd3f503e9ff1073b9b39bfe3e42
akonya <akonya1@gmail.com>
2013-12-12 15:28:26 -0500

    test parameters

--- LcSimParameters.h
+++ LcSimParameters.h
@@ -3,15 +3,15 @@
 
 #define PI 3.141592654
 
-#define ISIZE 20     //lattice size in i (x) direction
-#define JSIZE 20     //lattice size in j (y) direction
-#define KSIZE 20     //lattice size in k (z) direction
+#define ISIZE 30     //lattice size in i (x) direction
+#define JSIZE 30     //lattice size in j (y) direction
+#define KSIZE 30     //lattice size in k (z) direction
 
 #define PITCH 10     //pitch in units of lattice spacing
 #define QZERO 2.0*PI/PITCH    //2*PI/pitch
 
 #define DELTAT 0.0001    //timestep
-#define NSTEPS 100000    //total steps
+#define NSTEPS 10000    //total steps
 #define FRAMERATE 1000   //how many steps between frames
 
 #endif
